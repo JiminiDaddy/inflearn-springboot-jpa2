273ed945531b06ef5e335a8e87ffeb0b7634653a
chpark0904 <chpark0904@gmail.com>
2021-07-02 12:01:33 +0900

    feat: mustache 의존성 추가 및 샘플용 index 페이지 구현
    TODO UI 버튼 처리 및 form uri 설정 (현재 샘플용으로만 해놓음)

---
+++ src/main/java/jpabook/shop/controller/OrderController.java
@@ -0,0 +1,16 @@
+package jpabook.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OrderController {
+	/* Get으로 / 요청이 올 경우 기본적으로 templates/index 파일이 실행됨:w
+
+	 *
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+	*/
+}
